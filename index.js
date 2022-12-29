80f3f6ba462d7d37418237ed7b2b6efe61400f1e
zhoaib <zhoaibhassan@gmail.com>
2022-12-29 18:01:17 +0600

    delete

--- index.js
+++ index.js
@@ -2,9 +2,45 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.m5zl65y.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const taskCollection = client.db('taskuser').collection('allTask')
+        app.post('/allTask', async (req, res) => {
+            const allTask = req.body
+            const result = await taskCollection.insertOne(allTask);
+            res.send(result)
+        });
+
+        app.get('/myTask', async (req, res) => {
+            const email = req.query.email
+            const query = { userEmail: email };
+            const result = await taskCollection.find(query).toArray();
+            res.send(result)
+        });
+
+        app.delete('/myTask/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) };
+            const result = await taskCollection.deleteOne(filter);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('task manager server')
